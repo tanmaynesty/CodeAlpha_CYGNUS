ac04f5a973da60f761bcd6f2dd6dde378cf6862b
Tanmay <tanmaynesty@gmail.com>
2026-06-13 22:43:43 +0530

    Fix Vercel bash compatibility for venv

--- build_files.sh
+++ build_files.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
+set -ex
 echo "BUILD START"
 
 # Create a virtual environment and activate it
 python3 -m venv venv
-source venv/bin/activate
+. venv/bin/activate
 
 # Install dependencies
 pip install -r requirements.txt
 
 # Run migrations and collectstatic
+mkdir -p staticfiles
 cd backend
-python manage.py collectstatic --noinput --clear
-python manage.py migrate
+python3 manage.py collectstatic --noinput --clear
+python3 manage.py migrate
 
 echo "BUILD END"
